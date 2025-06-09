10915f791c8b56e0a252a9e485c3c6739a69f1b1
lqyrmk <1638574981@qq.com>
2025-06-09 15:05:58 +0800

    feat: add a CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,12 +2,26 @@ import uvicorn
 
 from typing import Union
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routers import items
 
 app = FastAPI()
 
 app.include_router(items.router)
 
+origins = [
+    "http://localhost",
+    "http://localhost:9000",  # front-end
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
